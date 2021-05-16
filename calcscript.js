4d611c2cc5456f5f4505f349cde68561d3060320
Minnu <minnumariyaj@gmail.com>
2021-05-16 19:20:29 +0530

    calculator

---
+++ calcscript.js
@@ -0,0 +1,15 @@
+function displayBox(num) {
+    inpt.value+=num;
+}
+function evaluateExpression(){
+    let val=inpt.value;
+    let result=eval(val);
+    inpt.value=result;
+}
+function clearBox(){
+    inpt.value=""
+
+}
+function backSpace(){
+    inpt.value=inpt.value.slice(0,-1)
+}
